d4a6015415e2ef8ec42fc2e3e909010116b43773
Dennes Njoroge <dennesmwangi55@gmail.com>
2026-06-26 17:57:17 +0300

    feat: update SHIF calculation logic and remove income threshold configuration

--- src/api/modules/shif.js
+++ src/api/modules/shif.js
@@ -1,1 +1,10 @@
-export const calculateSHIF = (grossPay) => {};
+import shifRates from "../config/shif-rates.json" with { type: "json" };
+
+export const calculateSHIF = (grossPay) => {
+  // constants
+  const SHIF_RATE = shifRates["shif-rate"];
+  const SHIF_MIN_CONTRIBUTION = shifRates["shif-min-contribution"];
+
+  // return shif
+  return Math.max(grossPay * SHIF_RATE, SHIF_MIN_CONTRIBUTION);
+};
