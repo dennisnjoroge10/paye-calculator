12938eb211ff12dec555f83c9924a8fa96006c55
Dennes Njoroge <dennesmwangi55@gmail.com>
2026-06-26 18:13:02 +0300

    feat: implement housing levy calculation logic

--- src/api/modules/housingLevy.js
+++ src/api/modules/housingLevy.js
@@ -1,1 +1,8 @@
-export const calculateHousingLevy = (grossPay) => {};
+import housingLevyRates from "../config/housing-levy-rates.json" with { type: "json" };
+
+export const calculateHousingLevy = (grossPay) => {
+  // constants
+  const HOUSING_LEVY_RATE = housingLevyRates["housing-levy-rate"];
+
+  return grossPay * HOUSING_LEVY_RATE;
+};
